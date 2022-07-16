5a040a78f3e008ac2bd64b5fc4c825d57bc2d580
Arbaz Khan <arbaazkhanark23@gmail.com>
2022-07-16 16:21:56 +0530

    images added and also added dice_rolled

--- dice.js
+++ dice.js
@@ -1,1 +1,69 @@
 'use strict'
+
+
+//selecting elements
+const player1=document.querySelector('.p1');
+const player2=document.querySelector('.p2');
+
+const current_score1=document.querySelector('.cs1');
+const current_score2=document.querySelector('.cs2');
+
+const hold_score1=document.querySelector('.hs1');
+const hold_score2=document.querySelector('.hs2');
+
+const roll= document.querySelector('.roll');
+
+const hs1= document.querySelector('.hs1');
+const hs2=document.querySelector('.hs2');
+
+const dice=document.querySelector('.img');
+
+const dice_img=document.querySelector('.dice_roll');
+
+
+//starting conditions
+hs1.textContent=0;
+hs2.textContent=0;
+dice_img.classList.add('hidden');
+
+
+
+let current_score=0;
+const score=[0,0];
+let activePlayer=1;
+//Rolling dice functionality
+roll.addEventListener('click', function(){
+    console.log('Checked');
+    const rand=Math.trunc(Math.random()*6)+1;
+    console.log(rand);
+    // current_score1.textContent=rand;
+
+//Display dice
+dice_img.classList.remove('hidden');
+dice_img.src=`/img/img${roll}.png`;
+
+
+//Check for rolled 1: if true, switch to next player   
+if(rand !== 1){
+    //add current score and add dice roll(random number)
+    current_score+=rand;
+    
+    document.querySelector(`.cs${activePlayer}`).textContent= current_score;
+    // current_score1.textContent = current_score;
+}else{
+    //switch to next player
+    document.querySelector(`.cs${activePlayer}`).textContent= 0;
+    activePlayer= activePlayer ==1?2:1;
+    current_score=0;
+    
+    player1.classList.toggle('active');
+    player2.classList.toggle('active');
+       
+}
+
+
+
+});
+
+
+
